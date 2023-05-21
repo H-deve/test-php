f83d710395629611097f14d30240d57befa7011d
moatasm <Hajjo.dev@gmail.com>
2023-05-21 19:54:53 +0200

    adding the regsiter file to project

---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+
+class register extends CI_Controller{
+
+
+public function r_token(){
+
+$this->load->model("register_model");
+
+
+if(isset($_POST["Token"])){
+
+$token=$_POST["Token"];
+
+
+}
+}
+
+
+}
+
+
+
+
+
+?>
